4dd76422e634fa04f5e832f7091f63176df4eb5e
god-of-thunder <52755752+god-of-thunder@users.noreply.github.com>
2020-08-06 11:56:30 +0800

    Update settings.py

--- shopsite/settings.py
+++ shopsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8v5#==u7uh^v-%nwdtw$fi4*)v$h^yxnhhw=j5r&7ny0uqau%6'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
